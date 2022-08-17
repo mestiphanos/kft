d46e39bf4f76abb4a4f99fa9ee17c59b3cf58bf0
DiboraHaile <deborahaile3@gmail.com>
2022-08-17 14:40:25 +0300

    add: add files to the package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='kft-dp',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='Data pipeline for KFT',
+    long_description='',
+    long_description_content_type="text/markdown"
+    url="http://gitlab.kifiya.et/yabi/kft-data-lakehouse",
+    project_urls = {
+        "Bug Tracker": "http://gitlab.kifiya.et/yabi/kft-data-lakehouse/issues"
+    },
+    license='MIT',
+    packages=['kft-dp'],
+    install_requires=['s3fs','boto3','pandas'],
+)
