16bc5382c23113e65dd04c8353c76acee9212692
Dibora_H <dhaile@kifiya.et>
2022-08-19 12:28:02 +0000

    imp: enable data fetching using columns

--- kft_dp/fetch_data.py
+++ kft_dp/fetch_data.py
@@ -31,17 +31,97 @@ def return_partitions():
  'minilik']
     return partitions
 
+def return_columns(table_name='merged_business_data'):
+    table_info = {
+        "merged_business_data": 
+        ["index",
+        "dataset_name",
+        "ምርመራ",
+        "ስልክ ቁጥር",
+        "ተ.ቁ",
+        "ተቋሙ የሚጠቀም ግብዓት.አይነት",
+        "ተቋሙ የሚጠቀም ግብዓት.አይነት.1",
+        "ተቋሙ የሚጠቀም ግብዓት.የሀገር ውስጥ",
+        "ተቋሙ የሚጠቀም ግብዓት.የውጭ ሀገር",
+        "ተቋሙ የሚጠቀምበት ማሽን.ብዛት",
+        "ተቋሙ የሚጠቀምበት ማሽን.አይነት",
+        "ንዑስ ዘርፍ",
+        "ዋና ዘርፍ",
+        "የመስሪያ ቦታ ስፋት በካ.ሜ",
+        "የሚያመርቱት የምርት መጠን (በዓመት)",
+        "የሚያመርቱት የምርት ዓይነት",
+        "የሚያመርቱት የምርት ዓይነት.1",
+        "የሚፈልጉት የድጋፍ አይነት",
+        "የማምረቻ ቦታ ሁኔታ.በኪራይ",
+        "የማምረቻ ቦታ ሁኔታ.በግል",
+        "የማምረቻ ቦታ ሁኔታ.ፓርክ",
+        "የማምረቻ ቦታ ሁኔታ.ፓርክ.1",
+        "የተመሰረተበት ዓ.ም",
+        "የተቋሙ አድራሻ.ልዩ ቦታ",
+        "የተቋሙ አድራሻ.ክ/ከተማ",
+        "የተቋሙ አድራሻ.ወረዳ",
+        "የተፈጠረ የስራ ዕድል.ቋሚ.ሴ",
+        "የተፈጠረ የስራ ዕድል.ቋሚ.ወ",
+        "የተፈጠረ የስራ ዕድል.ቋሚ.ድ",
+        "የተፈጠረ የስራ ዕድል.ጊዜያዊ.ሴ",
+        "የተፈጠረ የስራ ዕድል.ጊዜያዊ.ወ",
+        "የተፈጠረ የስራ ዕድል.ጊዜያዊ.ድ",
+        "የአደረጃጀት ዓይነት",
+        "የኢንዱስትሪ ስም",
+        "የካፒታል ሁኔታ.መነሻ",
+        "የካፒታል ሁኔታ.አሁን ያለው",
+        "የድርጅቱ አባላት ሁኔታ.ሲቋቋም.ሴ",
+        "የድርጅቱ አባላት ሁኔታ.ሲቋቋም.ወ",
+        "የድርጅቱ አባላት ሁኔታ.ሲቋቋም.ድ",
+        "የድርጅቱ አባላት ሁኔታ.አሁን.ሴ",
+        "የድርጅቱ አባላት ሁኔታ.አሁን.ወ",
+        "የድርጅቱ አባላት ሁኔታ.አሁን.ድ",
+        "የድርጅት ባለቤት መረጃ.ስም",
+        "የድርጅት ባለቤት መረጃ.ዕድሜ",
+        "የድርጅት ባለቤት መረጃ.ፆታ",
+        "የገበያ መዳረሻ.የሀገር ውስጥ",
+        "የገበያ መዳረሻ.የውጭ ሀገር",
+        "የገብያ ሁኔታ.በሀገር ውስጥ",
+        "የገብያ ሁኔታ.በውጭ ሀገር",
+        "የድርጅት መረጃ.ክልል",
+        "የድርጅት መረጃ.ዞን/ክፍለ ከተማ",
+        "የድርጅት መረጃ.ወረዳ",
+        "የድርጅት መረጃ.ከተማ",
+        "የድርጅት መረጃ.ልዩ ቦታ",
+        "ፍቃድ ያገኘበት ዓ.ም",
+        "የተሰማራበት የስራ መስክ",
+        "የኢንዱስትሪው ዓይነት (በትርጓሜ)",
+        "የግብር ከፋይ መለያ ቁጥር",
+        "የአባላት (ባለቤት) ብዛት.ወ",
+        "የአባላት (ባለቤት) ብዛት.ሴ",
+        "የአባላት (ባለቤት) ብዛት.ድ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ቋሚ.ወ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ቋሚ.ሴ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ቋሚ.ድ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ጊዜያዊ.ወ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ጊዜያዊ.ሴ",
+        "ከአባላት (ባለቤት) ውጭ የተፈጠረ የስራ ዕድል.ጊዜያዊ.ድ",
+        "ጠቅላላ የተፈጠ የስራ ዕድል.ቋሚ *.ወ",
+        "ጠቅላላ የተፈጠ የስራ ዕድል.ቋሚ *.ሴ",
+        "ጠቅላላ የተፈጠ የስራ ዕድል.ቋሚ *.ድ",
+        "የኢንዱስትሪው ዓይነት"]
+    }
+    return table_info[table_name]
+
 def return_tables():
     return 'merged_business_data'
 
-def fetch(dry_run = False,table_name='merged_business_data',return_type='dataframe',output_location="kft_query_output",dataset_names=[]):
-    query = f"""SELECT 
-                *
-                FROM
-                {table_name}
-             """
+def fetch(dry_run = False,table_name='merged_business_data',cols = [],return_type='dataframe',output_location="kft_query_output",dataset_names=[]):
+    logger.info("If you want to fetch cols from the table, you can choose from the following:")
+    logger.info(return_columns(table_name))
+    logger.info("")
+    query = "SELECT "
+    selected_cols = "*"
+    if cols:
+        selected_cols = (" ,").join([f""" "{col}" """ for col in cols])  
+    query += selected_cols + f"  FROM {table_name}"
     if dataset_names:
-        query += "WHERE "+(" or ").join([f"partition_0 = '{dataset_name}'" for dataset_name in dataset_names])
+        query += " WHERE "+(" or ").join([f"partition_0 = '{dataset_name}'" for dataset_name in dataset_names])
     else:
         logger.info("Insert data_set name:")
         logger.info("Choose one/more of the following")
