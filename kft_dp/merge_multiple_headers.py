3bcb29ace046ca2a28a82d5e814529400cc2a6b5
Dibora_H <dhaile@kifiya.et>
2022-09-06 11:04:01 +0000

    minor_chng : Minor change on using helper methods to load file

--- kft_dp/merge_multiple_headers.py
+++ kft_dp/merge_multiple_headers.py
@@ -1,10 +1,13 @@
+from kft_dp.helper_methods import *
+
 class MergeMultipleHeaders:
     """
     A class that corrects and 
-    formats multiple headers
+-    formats multiple headers
     """
     def __init__(self,df,col_start_idx = 0,col_start_val = 'ተ.ቁ'):
         self.df = df
+        print(col_start_idx,col_start_val)
         self.set_header_idx(col_start_idx,col_start_val)
         
         
@@ -18,7 +21,7 @@ class MergeMultipleHeaders:
             columns.append(('.').join([str(item) for item in column if not str(item).startswith("Unnamed") and str(item) != 'nan']))
         return columns
 
-    def get_df(self,filename,s_name,header_start=0,header_end=0):
+    def get_df(self,filepath,s_name,header_start=0,header_end=0):
         """
         Get the dataframe
         """
@@ -27,8 +30,12 @@ class MergeMultipleHeaders:
         if header_end:
             self.header_end = header_end
         
-
-        self.df = pd.read_excel(filename,engine='openpyxl',header=list(range(self.header_start,self.header_end)),sheet_name=s_name)
+        
+        # self.df.columns = self.df.loc[self.header_start:self.header_end-1,:].values.tolist()
+        # self.df.drop(range(0,self.header_end), axis=0, inplace=True)
+        header = list(range(self.header_start,self.header_end))
+        self.df = return_sheet_data(filepath,s_name,header)
+        # self.df = pd.read_excel(filename,engine='openpyxl',header=,sheet_name=s_name)
         
         if self.header_start != self.header_end:
             columns = self.get_correct_format_columns()
@@ -37,15 +44,11 @@ class MergeMultipleHeaders:
         return self.df
 
     def set_header_idx(self,col_start_idx,col_start_val):
-        """
-        set indexes for start
-        and end of multiple
-        headers
-        """
         if not self.df.empty:
+            print(col_start_idx,col_start_val)
             if not self.df[self.df[col_start_idx] == col_start_val].empty:
                 # the index where the manually set column name appears
-                self.header_start = self.df.index[df[col_start_idx] == col_start_val].tolist()[0]
+                self.header_start = self.df.index[self.df[col_start_idx] == col_start_val].tolist()[0]
                 # the new dataframe starting from the column name given
                 df_new = self.df.loc[self.header_start:,:]
                 # check if the second value that is not null from the chosen column
@@ -63,4 +66,4 @@ class MergeMultipleHeaders:
                 print(self.df.head())
                 exit()
           
-
+        # return self.df
