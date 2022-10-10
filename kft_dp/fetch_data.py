5c218e0ae016ef04d6d9a6bb2acd49fb57fd94d6
Dibora_H <dhaile@kifiya.et>
2022-10-10 11:09:11 +0000

    update: Enabled fetching data from stage_0 and stage_1 data sources

--- kft_dp/fetch_data.py
+++ kft_dp/fetch_data.py
@@ -3,13 +3,30 @@ from kft_dp.logger import Logger
 from kft_dp.table_info import *
 from kft_dp.schema_registry import *
 import pandas as pd
+import s3fs
+
+
 logger = Logger(__name__).log()
+source_info = SchemaRegistry().return_sources()
 
 def fetch_data_s3(path):
-    try:
-        return pd.read_csv(path)
-    except:
-        logger.error(f"Error reading the file {path}")
+    if path.endswith(".csv"):
+        try:
+            return pd.read_csv(path)
+        except:
+            logger.error(f"Error reading the file {path}")
+    elif path.endswith("/"):
+        s3 = s3fs.S3FileSystem(anon=False)
+        files = s3.glob(f"{path}*.csv")
+        df_list = []
+        for file in files:
+            df = pd.read_csv(f"s3://{file}")
+            df["dataset_name"] = file.split("/")[-1].replace(".csv",'')
+            df_list.append(df)
+        df = pd.concat(df_list)
+        return df
+
+
 
 
 class FetchDataAthena:
@@ -101,20 +118,90 @@ class FetchDataAthena:
 
 
 def list_sources(stage=''):
-    return SchemaRegistry().return_sources()
+        """ Lists all the available sources we have on
+            the data lake in 'stage_0' and 'stage_1 :
+            Parameters
+            ----------
+            stage : string 
+                The stage the data sources reside in, which 
+                are currently either 'stage_0' or 'stage_1'
+                'stage_0' : contains raw data
+                'stage_1' : contains cleaned data
 
+            Returns
+            -------
+            source_names : dict
+                Source names found under the stages.
+        """
+        
+        if stage:
+            source_names = {stage : list(source_info.keys())}
+        else:
+            # source_names = [list(schema_name.keys()) for schema_name in ]
+            source_names = {}
+            for key in source_info.keys():
+                source_names[key] = list(source_info[key].keys())
 
-def get_source(stage,source_name):
+            return source_names
+
+def get_details(stage='',source_name=''):
+    """ Get detailed information about the provided data
+        source from the data lake which can be either 
+        raw/cleaned or in our case 'stage_0' or 'stage_1' :
+        Parameters
+        ----------
+        stage : string 
+            The stage the data sources reside in, which 
+            are currently either 'stage_0' or 'stage_1'
+            'stage_0' : contains raw data
+            'stage_1' : contains cleaned data
+        source_name : string
+            The name of the source to be fetched, source_name
+            is schema_name for raw/stage_0 data sources and
+            table_name for cleaned/stage_1 data sources. All
+            available sources name can be found by calling 
+            list_sources().
+
+        Returns
+        -------
+        detailed_source_info : dict
+            Detailed schema information.
+    """
+    detailed_source_info = source_info[stage][source_name]
+    return detailed_source_info
+
+def get_data(stage,source_name):
+    """ Fetch the source provided from the data lake 
+        which can be either raw/cleaned or in our case
+        'stage_0' or 'stage_1' :
+        Parameters
+        ----------
+        stage : string 
+            The stage the data sources reside in, which 
+            are currently either 'stage_0' or 'stage_1'
+            'stage_0' : contains raw data
+            'stage_1' : contains cleaned data
+        source_name : string
+            The name of the source to be fetched, source_name
+            is schema_name for raw/stage_0 data sources and
+            table_name for cleaned/stage_1 data sources. All
+            available sources name can be found by calling 
+            list_sources().
+
+        Returns
+        -------
+        df : DataFrame
+            Data Fetched.
+    """
     if stage == 'stage_0':
-        path = list_sources(stage)[source_name]
+        path = source_info[stage][source_name]['path']
         if path:
-            return fetch_data_s3(path)
+            df = fetch_data_s3(path)
+            return df
     elif stage == 'stage_1':
-        sources = list_sources(stage)
-        print(sources)
-        print(source_name)
-
-        db_name = [key for key in sources[stage].keys() if source_name in sources[stage][key]][0]
-        return FetchDataAthena(table_name=source_name,database_name=db_name).run()
-
-
+        db_name = source_info[stage][source_name]['DatabaseName']
+        try:
+            df = FetchDataAthena(table_name=source_name,database_name=db_name).run()
+        except:
+            df = fetch_data_s3(source_info[stage][source_name]['StorageDescriptor']['Location'])
+        return df
