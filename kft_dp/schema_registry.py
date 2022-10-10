1b0e5e6b7c35b5647608910ad2a785c0b1c042f1
Dibora_H <dhaile@kifiya.et>
2022-10-10 11:06:40 +0000

    feat: add methods to getschema information from schema registry

--- kft_dp/schema_registry.py
+++ kft_dp/schema_registry.py
@@ -9,11 +9,12 @@ class SchemaRegistry(Glue):
     schemas found in the schema
     registry
     """
-    def __init__(self,dry_run,region_name='eu-west-1',schema_name,registry_name,**kwargs):
+    def __init__(self,registry_name='Stage_0',schema_name='',region_name='us-east-1',dry_run=False,**kwargs):
         self.schema_name = schema_name
         self.registry_name = registry_name
         self.additional_params = kwargs
-        super().__init__(dry_run,region_name):
+        self.sources = {}
+        super().__init__(dry_run,region_name)
         
     def create_registry(self):
         """
@@ -39,26 +40,55 @@ class SchemaRegistry(Glue):
         DataFormat='JSON',
         Compatibility='FULL',
         Description=self.additional_params['schema']['description'],
-        Tags= self.additional_params["schema"]['tags']
+        Tags= self.additional_params["schema"]['tags'],
         SchemaDefinition= schema_info
         )
         return response
 
-    def get_schema(self):
+    def get_schema(self,schema_name='',latest_version=True,version_number=1):
         """
         Get the schema information
         from the schema registry
         using the version and the
         schema name
         """
-        latest_version = self.additional_params['schema']['latest_version']
+        if schema_name:
+            self.schema_name = schema_name
+        # latest_version = self.additional_params['schema']['latest_version']
         if not latest_version:
             version_number = self.additional_params['schema']['version_numb']
-            
-        schema_version_info= dict('LatestVersion': latest_version,
-                                  'VersionNumber': version_number)
+            schema_version_info ={'VersionNumber': version_number}
+        else:   
+            schema_version_info= {'LatestVersion': latest_version}
+        
         schema_info = json.loads(self.client.get_schema_version(
         SchemaId={'SchemaName': self.schema_name,
             'RegistryName': self.registry_name
-        }SchemaVersionNumber=schema_version_info)['SchemaDefinition'])
+        },
+        SchemaVersionNumber=schema_version_info)['SchemaDefinition'])
         return schema_info
+    
+    def list_schemas(self):
+        """
+        list_schemas
+        """
+        response = self.client.list_schemas(
+        RegistryId={
+            'RegistryName': self.registry_name
+            
+        }
+        )
+        return response
+    
+
+    def list_schema_info(self):
+        schema_info = {schema['SchemaName']: self.get_schema(schema['SchemaName']) for schema in self.list_schemas()['Schemas']}
+        return schema_info    
+
+    def return_sources(self,stage=''):
+        sources = {'stage_0': self.list_schema_info(),
+                'stage_1': self.return_table_info()}
+        if stage:
+            return sources[stage]
+        else:
+            return sources
