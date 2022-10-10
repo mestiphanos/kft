0cca944ca7766ae146f134dd02ca2af07e525234
Dibora_H <dhaile@kifiya.et>
2022-10-10 11:35:41 +0000

    test: testing main methods exposed in __init__.py file

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from kft_dp.fetch_data import *
+import kft_dp
 
 if __name__ == '__main__':
-    fd = FetchData(dry_run=False,table_name='mse_data')
-    print(fd.get_detail_info_tables())
-    print(fd.get_detail_info_table())
+    # for listing all the source file names 
+    print(kft_dp.list_sources())
+
+    # get details of each source
+    print(kft_dp.get_details('stage_1','payment'))
+    print(kft_dp.get_details('stage_0','work_stopped.csv'))
+    
+    # get documentation for the methods exposed in __init__
+    print(help(kft_dp))
+
+    # get data by specifying the stage and source_name 
+    print(kft_dp.get_data(stage='stage_0',source_name ='work_stopped.csv'))
 
